38ea0609143920fa1eb15cfaf67fd1fb7c1c656c
ibsonreis <145687759+ibsonreis@users.noreply.github.com>
2026-03-19 11:29:48 +0000

    Add checkbox state and update layout/text
    Introduce local state for the Checkbox using useState and convert it to a controlled component (checked + onCheckedChange). Adjust component layout and spacing classes, update the item text and category, and normalize import quote/style formatting for consistency.

--- app/index.tsx
+++ app/index.tsx
@@ -1,19 +1,22 @@
-import { View } from "react-native";
-import { Text } from "@/components/ui/text"
-import { Checkbox } from "@/components/ui/checkbox"
+import { View } from 'react-native';
+import { Text } from '@/components/ui/text';
+import { Checkbox } from '@/components/ui/checkbox';
+import { useState } from 'react';
 
 export default function HomeScreen() {
+  const [checked, setChecked] = useState(false);
+
   return (
-    <View className="flex flex-1 py-32 bg-background">
+    <View className="bg-background flex flex-1 py-32">
       <Text className="text-foreground text-center">Hello, world!</Text>
 
-      <View className="w-full flex flex-row mx-5 gap-5 py-4">
-        <View className="flex justify-center items-center ">
-          <Checkbox className="border-2"></Checkbox>
+      <View className="mx-3 flex w-full flex-row gap-5 py-4">
+        <View className="flex items-center justify-center">
+          <Checkbox className="border-2" onCheckedChange={setChecked} checked={checked}></Checkbox>
         </View>
-        <View className="border-b border-foreground-transparent pb-">
-          <Text className="text-foreground">Submit Assignment</Text>
-          <Text className="text-foreground-transparent">Due: Oct 20</Text>
+        <View className="border-foreground-transparent mr-10 flex h-full flex-1 gap-1 border-b py-4">
+          <Text className="text-foreground text-xl">My test item</Text>
+          <Text className="text-foreground-transparent text-xl">Test category</Text>
         </View>
       </View>
     </View>
